22b22ab61c9db651c48f5d58bf131d45be120595
Audace Nakeshimana <audnakeshimana@gmail.com>
2017-09-03 13:54:32 -0400

    updating data modifiers

--- project/data_modifiers.py
+++ project/data_modifiers.py
@@ -1,7 +1,25 @@
+import os
+import add_question
+
 def add_question(quiz_id, round_id, question):
-    with open("quiz"+str(quiz_id)+".json") as quizfile:
-        quizData = json.loads(quizfile.read())
-    quizData["rounds"][round_id]["questions"].append(question);
-    with open(quizfilename) as quizfile:
-        quiz_file = quiz_data
-    quizfilename.close()
+    question_dict = {};
+    question_dict["title"] = question["title"]
+    question_dict["id"] = question["id"]
+    question_dict["answer"] = question["answer"]
+    question_dict["extra_options"] = question["extra_options"]
+    question_dict["quiz_name"] = question["quiz_name"]
+    question_dict["round_name"] = question["round_name"]
+    question_json = json.dumps(question_dict,sort_keys=True)
+    filename = "data/question_"+question_dict["id"]+".json"
+
+    with open(filename) as question_stream:
+        json.loads(question_stream, question_json)
+    question_stream.close()
+
+def add_questions(questions):
+    for question in questions:
+        add_question(question)
+def delete_question(question_id):
+    file_name = "data/question_"+question_id+".json"
+    os.unlink(file_name)
+
