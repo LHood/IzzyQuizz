b1200ba4096b09eaf27a6d45823f82665f4b005f
Audace Nakeshimana <audnakeshimana@gmail.com>
2017-09-19 09:31:40 -0400

    changed data modifies and retrievers

--- project/data_modifiers.py
+++ project/data_modifiers.py
@@ -1,37 +1,46 @@
 import os
 import json
+import time
+import tools
 
-def add_question(quiz_id, round_id, question):
+def add_question(title, answer, extra_options):
     question_dict = {};
-    question_dict["title"] = question["title"]
-    question_dict["id"] = question["id"]
-    question_dict["answer"] = question["answer"]
-    question_dict["extra_options"] = question["extra_options"]
-    question_dict["quiz_name"] = question["quiz_name"]
-    question_dict["round_name"] = question["round_name"]
+    question_dict["title"] = title
+    question_dict["id"] = tools.current_milli_time()
+    question_dict["answer"] = answer
+    question_dict["extra_options"] = extra_options
     question_json = json.dumps(question_dict,sort_keys=True)
     filename = "data/question_"+question_dict["id"]+".json"
 
-    with open(filename) as question_stream:
-        json.loads(question_stream, question_json)
-    question_stream.close()
+    with open(filename, 'w') as f:
+        json.loads(f, question_json)
+    f.close()
+    return question_json
 
 def add_questions(questions):
     for question in questions:
-        add_question(question)
+        add_question(question[0], question[1], question[2])
+
 def delete_question(question_id):
     file_name = "data/question_"+question_id+".json"
     os.unlink(file_name)
 
 def add_result(result):
-    result_dict = {}
-    result_dict["user_id"] = result.user_id
-    result_dict["user_email"] = result.user_email
-    result_dict["question_id"] = result.question_id
-    result_dict["points"] = result.points
+    result = json.dumps(result)
+    file_name = "data/result_user_"+result.user_id+"_question__"+result.question_id
+    with open(file_name, "w") as f:
+        json.dumps(f, result)
+    f.close()
 
-    result_json = json.dumps(result_dict, sort_keys=True)
-    file_name = "data/result_user_id_is_"+result.user_id+"_question_id_is_"+result.question_id
-    with open(file_name, "w") as result_stream:
-        json.dumps(result_stream, result_json)
-    result_stream.close()
+def add_user(user_id, first_name, last_name, email):
+    user = {}
+    user["id"] = user_id
+    user["first_name"] = first_name
+    user["last_name"]  = last_name
+    user["email"] = email
+    with open('data/user_'+user_id) as f:
+        json.loads(f, json.dumps(user, sort_keys=True))
+    f.close()
+    return json.dumps(user)
+
+
--- project/data_retrievers.py
+++ project/data_retrievers.py
@@ -1,9 +1,14 @@
 import os
 import path
-
-def list_questions():
-    questions = glob.glob("data/question_*")
-    return questions
+import json
+def get_questions():
+    filenames = glob.glob("data/question_*")
+    questions = []
+    for filename in filenames:
+        with open("data/"+filename) as f:
+            question = json.load(f)
+        questions.append(question)
+    return json.dumps(questions)
 
 def list_results():
     results = glob.glob("data/result_*")
