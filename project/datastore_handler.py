68ba91e47c9e2a0b7aac34dd63e790f70404038d
Audace Nakeshimana <audnakeshimana@gmail.com>
2017-09-30 18:17:12 -0400

    datastore handler

--- project/datastore_handler.py
+++ project/datastore_handler.py
@@ -1,16 +1,26 @@
 from google.cloud import datastore
+import time
 
 datastore_client = datastore.Client()
 
-kind = 'Task'
+def save_user(userId, userName):
+	kind = 'user'
+	name = str(int(round(time.time() * 1000)))
+	task_key = datastore_client.key(str(userId), userName)
+	task = datastore.Entity(key=task_key)
+	task['id'] = userId
+	task['name'] = userName
+	datastore_client.put(task)
+	print('Saved {}:{}'.format(task.key.name, task['id']))
+users = [(1, 'Nakeshimana'), (2, 'Audace')]
+for user in users:
+	save_user(user[0], user[1])
 
-name = 'sampletask1'
+def list_users():
+	query = datastore_client.query()
+	results = list(query.fetch())
+	for result in results:
+		print('result: ', result)
+	return results
 
-task_key = datastore_client.key(kind, name)
-
-task = datastore.Entity(key=task_key)
-task['description'] = 'Buy Milk'
-
-datastore_client.put(task)
-
-print('Saved {}:{}'.format(task.key.name, task['description']))
+list_users()
