6dfe03b3f723977e30807267e4e1b4ab1586f0d6
Audace Nakeshimana <audnakeshimana@gmail.com>
2017-10-03 09:06:52 -0400

    doing stuff

--- project/datastore_handler.py
+++ project/datastore_handler.py
@@ -5,22 +5,34 @@ datastore_client = datastore.Client()
 
 def save_user(userId, userName):
 	kind = 'user'
-	name = str(int(round(time.time() * 1000)))
-	task_key = datastore_client.key(str(userId), userName)
+	task_key = datastore_client.key(kind, userId)
 	task = datastore.Entity(key=task_key)
 	task['id'] = userId
 	task['name'] = userName
 	datastore_client.put(task)
-	print('Saved {}:{}'.format(task.key.name, task['id']))
-users = [(1, 'Nakeshimana'), (2, 'Audace')]
-for user in users:
-	save_user(user[0], user[1])
-
+	print('Saved {}:{}'.format(task.key.kind, task.key.id))
 def list_users():
-	query = datastore_client.query()
+	query = datastore_client.query(kind='user')
 	results = list(query.fetch())
 	for result in results:
-		print('result: ', result)
-	return results
-
+		name, userId = 'user', result['id']
+		task_key = datastore_client.key(name, str(userId))
+		print('listed: ', task_key)
+	response = [[result['id'], result['name']] for result in results]
+	print(response)
+	return response
 list_users()
+
+def delete_users():
+	query = datastore_client.query(kind='user')
+	results = list(query.fetch())
+	for result in results:
+		name, userId = 'user', result['id']
+		task_key = datastore_client.key(name, str(userId))
+		datastore.delete(task_key)
+		print('deleted: ', task_key)
+	results_ = list(query.fetch())
+	assert(results != results_)
+	print('after deletion: ', results)
+	return results
+delete_users()
