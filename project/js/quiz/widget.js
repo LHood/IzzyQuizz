d7bca062063f16de7549826e1c0cf06a9e379701
Audace Nakeshimana <audace@mit.edu>
2017-10-25 23:56:04 -0400

    questions displayin

--- project/js/quiz/widget.js
+++ project/js/quiz/widget.js
@@ -20,7 +20,8 @@ class userWidget {
 				{'className': 'logout-button waves-effect waves-light btn', href: '/logout'}, 'logout');
 			const userHolder = goog.dom.createDom('div', {className: 'user-holder z-depth-1'}, [userImage, nameHolder, logoutButton]);
 			const mainHolder = goog.dom.createDom('div', null, [userHolder]);
-			document.body.append(mainHolder);
+			const userDataNode = document.getElementById('user_data');
+			userDataNode.append(mainHolder);
 		});
 	}
 }
@@ -33,37 +34,50 @@ class quizData {
 	}
 
 	load_status() {
-
+		console.log('loading status');
 		return new Promise((resolve) => {
+			
 			XhrService.getJSON('/quiz/status').then( (response) => {
-				this.current_status = response; Promise.resolve(response)})
-				.then((response) => {
-					resolve(response)});
-		});	
+				this.current_status = response; console.log('-- got status response ', response); resolve(response)});
+		});
 	}
 	load_all_questions () {
-	
+	console.log('loading all questions');	
 		return new Promise((resolve) => {
-			XhrService.getJSON('/questions/all').then( (response) => {this.all_questions = response}).then(() => {Promise.resolve('done')});
+			XhrService.getJSON('/questions/all')
+			.then((response) => {this.all_questions = response; console.log('all_done'); resolve('done');});
 		});
 	}
 	load_current_questions () {
+	console.log('loading current questions');
 		return new Promise((resolve) => {
-			XhrService.getJSON('/questions/current').then((response) => {this.current_questions = response}).then(() => {Promise.resolve('done')});
-		})
+			XhrService.getJSON('/questions/current')
+			.then((response) => {this.current_questions = response; console.log('current_done'); resolve('done');});
+		});
 	}
 	load_all() {
-		return Promise.all([this.load_status(), this.load_all_questions(), this.load_current_questions()]);
-	}	
+		return new Promise((resolve) => {
+			resolve(Promise.all([this.load_status(), this.load_all_questions(), this.load_current_questions()]));
+		});
+	}
 }
 
-class quizDisplay() {
-	this.quiz_data = new quizData();
+class quizDisplay {
+	constructor () {
+		this.quiz_data = new quizData();
+	}
+
 	build(target) {
-		return new Promise ((resolve) {this.quiz_data.load_all()}).then((response){this.display_quiz(target)});
+		console.log('initializing quiz build');
+		return new Promise((resolve) => {
+			this.quiz_data.load_all().then(() => {console.log('resolved all data'); resolve('done');});
+		}).then((response) => {
+			console.log('goint to display quiz')
+			this.display_quiz(target)});
 	}
 	// Target is the target element where the quiz should be displayed in. (It is a Node element)
 	display_quiz(target){
+		console.log('initializing display');
 		const all_questions = this.quiz_data.all_questions;
 		const current_questions = this.quiz_data.current_questions;
 		const current_status = this.quiz_data.current_status;
@@ -76,26 +90,30 @@ class quizDisplay() {
 
 	}
 	generate_gadgets(questions) {
+		console.log('generating gadgets')
 		const results = []
-		for(question of questions ) {
+		for(const question of questions ) {
 		
 			results.push(this.generate_gadget(question));
 		}
 		return results;
 	}
+
 	generate_gadget(question) {
+		console.log('generating gadget for question ');
 
 		const title = question.title
+		const titleElement = goog.dom.createDom('h5', {}, title);
 		const options = question.options
-		const answer = questions.answer
+		const answer = question.answer
 		const shuffled_options = this.shuffle_array(options.concat([answer]))
 		const optionsElement = this.generate_options_element(question);
-		const gadget = goog.dom.createDom('div', {className: 'question_gadget'}, [titleElement, optionsElement]);
+		const gadget = goog.dom.createDom('div', {className: 'question_gadget z-depth-1'}, [titleElement, optionsElement]);
 		return gadget
 	}
 	//Courtesy of stackoverflow
 	// On link https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
-	shuffle(a) {
+	shuffle_array(a) {
 		for (let i = a.length - 1; i > 0; i--){
 			const j = Math.floor(Math.random() * i+1);
 			[a[i],a[j]] = [a[j],a[i]];
@@ -103,19 +121,32 @@ class quizDisplay() {
 	}
 	generate_options_element(question) {
 		const all_options = question.options.concat([question.answer])
-		this.shuffle(all_options);
-		option_elements = []
-		for(option of all_options) {
-			const element = goog.dom.createDom('input', {className: 'answer_option', type: 'radio', value: option, target_question: question.created_at}, option)
+		this.shuffle_array(all_options);
+		const option_elements = []
+		for(const option of all_options) {
+			const hash_value = question.created_at.toString()  + "_" + option.length.toString();
+			const selector = goog.dom.createDom('input', {name: 'answer_for' + question.created_at.toString(), 
+				id: hash_value, className: 'answer_option', type: 'radio', value: option, 
+				target_question: question.created_at,}, option);
+			const label = goog.dom.createDom('label', {for: hash_value}, option);
+			const element = goog.dom.createDom('div', {}, [selector, label]);
 			option_elements.push(element)
 		}
-		const result =  goog.dom.createDom('div', {className: 'options_holder'}, option_elements);
+		const result =  goog.dom.createDom('div', {id: 'options_holder_'+question.created_at.toString(), 
+			className: 'options_holder'}, option_elements);
+		console.log('returning options element', result);
 		return result
 	}
 	generate_submit_button() {
-		const button = goog.dom.createDom('a', {className: 'button waves-effect waves-light button'}, 'Submit');
+		const button = goog.dom.createDom('a', { id: 'submit_button', className: 'btn waves-effect waves-light button'}, 'Submit');
 		button.addEventListener('click', () => {window.grade_quiz()});
 		return button
 	}
 }
 
+userWidgetObject = new userWidget()
+userWidgetObject.createContent()
+
+const quizHolder = document.getElementById('quizHolder')
+const quizDisplayObject = new quizDisplay();
+quizDisplayObject.build(quizHolder);
--- project/js/quiz_init.js
+++ project/js/quiz_init.js
@@ -1,6 +1,5 @@
 goog.require('tools.modules');
 const required_js = [
-				'/js/xhr_service.js',
 				'/js/oauth2_service.js',
 				'/js/data_service.js',
 			];
